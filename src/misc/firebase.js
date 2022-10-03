ad2a1047e6895c637da7dac81445b949a66c6ede
Samarth Singh <samarthsingh6358@gmail.com>
2022-10-03 18:26:53 +0530

    initialized new firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyCIWXmH6u3LEfNHDVKFtO0DUm--g88SPdo',
+  authDomain: 'chat-app-10f7a.firebaseapp.com',
+  projectId: 'chat-app-10f7a',
+  storageBucket: 'chat-app-10f7a.appspot.com',
+  messagingSenderId: '761991523556',
+  appId: '1:761991523556:web:b8e6327ee55fd8446e10f1',
+};
+
+const app = firebase.initializeApp(config);
