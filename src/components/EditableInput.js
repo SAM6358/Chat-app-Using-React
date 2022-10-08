a1bf99b321739214b28f588cb7652b03fd7cf62c
Samarth Singh <samarthsingh6358@gmail.com>
2022-10-08 22:48:19 +0530

    created editable reusable input

---
+++ src/components/EditableInput.js
@@ -0,0 +1,59 @@
+import React, { useCallback, useState } from 'react';
+import { Alert, Icon, Input, InputGroup } from 'rsuite';
+
+const EditableInput = ({
+  initialValue,
+  onSave,
+  label = null,
+  placeholder = 'Write your name here',
+  emptyMsg = 'Input is empty',
+  ...inputProps
+}) => {
+  const [input, setInput] = useState(initialValue);
+  const [isEditable, setIsEditable] = useState(false);
+
+  const onInputChange = useCallback(value => {
+    setInput(value);
+  }, []);
+
+  const onEditClick = useCallback(() => {
+    setIsEditable(edit => !edit);
+    setInput(initialValue);
+  }, [initialValue]);
+
+  const onSaveClick = async () => {
+    const trimmed = input.trim();
+    if (trimmed === '') {
+      Alert.info(emptyMsg, 4000);
+    }
+    if (trimmed !== initialValue) {
+      await onSave(trimmed);
+    }
+    setIsEditable(false);
+  };
+
+  return (
+    <div>
+      {label}
+      <InputGroup>
+        <Input
+          {...inputProps}
+          placeholder={placeholder}
+          disabled={!isEditable}
+          value={input}
+          onChange={onInputChange}
+        />
+        <InputGroup.Button onClick={onEditClick}>
+          <Icon icon={isEditable ? 'close' : 'edit2'} />
+        </InputGroup.Button>
+        {isEditable && (
+          <InputGroup.Button onClick={onSaveClick}>
+            <Icon icon="check" />
+          </InputGroup.Button>
+        )}
+      </InputGroup>
+    </div>
+  );
+};
+
+export default EditableInput;
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import { Button, Drawer } from 'rsuite';
+import { Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import EditableInput from '../EditableInput';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
+
+  const onSave = async newData => {
+    console.log(newData);
+  };
 
   return (
     <>
@@ -12,6 +17,13 @@ const Dashboard = ({ onSignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         <h3>Hello, {profile.Name}</h3>
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.Name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
       </Drawer.Body>
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
