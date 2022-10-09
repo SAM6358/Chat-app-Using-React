89b379b35852d1dec67e25436c1045435a5b82e4
Samarth Singh <samarthsingh6358@gmail.com>
2022-10-09 13:51:50 +0530

    started to eork on Chat room list installed timeago-react

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { Divider } from 'rsuite';
 import CreateRoomBtnModal from './dashboard/CreateRoomBtnModal';
 import DashboardToggle from './dashboard/DashboardToggle';
+import ChatRoomList from './rooms/ChatRoomList';
 
 const Sidebar = () => {
+  const topSidebarRef = useRef();
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    if (topSidebarRef.current) {
+      setHeight(topSidebarRef.current.scrollHeight);
+    }
+  }, [topSidebarRef]);
+
   return (
     <div className="h-100 pt-2">
-      <div>
+      <div ref={topSidebarRef}>
         <DashboardToggle />
         <CreateRoomBtnModal />
+        <Divider>Join Conversation</Divider>
       </div>
+      <ChatRoomList aboveElementHeight={height} />
     </div>
   );
 };
