36a2d0cb41756b58d111694d0bb45723d0aa61ac
Samarth Singh <samarthsingh6358@gmail.com>
2022-10-10 06:04:53 +0530

    displayed user data with modal window

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
 import ProfileAvatar from '../../ProfileAvatar';
+import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
 const MessageItem = ({ message }) => {
   const { author, createdAt, text } = message;
@@ -14,7 +15,11 @@ const MessageItem = ({ message }) => {
           className="ml-1"
           size="md"
         />
-        <span className="ml-2">{author.Name}</span>
+        <ProfileInfoBtnModal
+          profile={author}
+          appearance="link"
+          className="pd-0 ml-1 text-black"
+        />
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
---
+++ src/components/chat-window/messages/ProfileInfoBtnModal.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Button, Modal } from 'rsuite';
+import { useModelState } from '../../../misc/custom-hooks';
+import ProfileAvatar from '../../ProfileAvatar';
+
+const ProfileInfoBtnModal = ({ profile, ...btnProps }) => {
+  const { isOpen, open, close } = useModelState();
+  const shortName = profile.Name.split(' ')[0];
+
+  const { Name, createdAt, avatar } = profile;
+
+  const memberSince = new Date(createdAt).toLocaleDateString();
+
+  return (
+    <div>
+      <Button {...btnProps} onClick={open}>
+        {shortName}
+      </Button>
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          <Modal.Title>{shortName} Profile</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="text-center">
+          <ProfileAvatar
+            src={avatar}
+            Name={Name}
+            className="width-200 height-200 img-fullsize font-huge"
+          />
+          <h4 className="mt-2">{Name}</h4>
+          <p>Member since : {memberSince} </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button block onClick={close}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+};
+
+export default ProfileInfoBtnModal;
